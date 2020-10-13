21bd7684b759ac2a94b54cb962374e844a3885c4
ca3hh <carter.angelus@richmond.edu>
2020-10-13 17:02:22 -0400

    added lines to VectorTester.cpp under DoubleVector

--- VectorTester.cpp
+++ VectorTester.cpp
@@ -25,7 +25,7 @@ int main()
    std::cout << std::endl;
    std::cout << dv.get(0)  << std::endl;
    std::cout << dv.size() << std::endl;
-   std::cout <<  << std::endl;
+   std::cout << dv.get(1) << std::endl;
 
    //-------------------------------------------------------------------------
 
